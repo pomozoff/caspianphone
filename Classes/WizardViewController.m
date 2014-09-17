0347f4646a14dd8b5f80afedf34ea8289eceae8c
Anton Pomozov <pomozoff@me.com>
2014-09-17 17:01:52 +0400

    [Add]: Remove 00 and + from phone number at signing in, GitHub issue #24

--- Classes/WizardViewController.m
+++ Classes/WizardViewController.m
@@ -749,7 +749,7 @@ static UICompositeViewDescription *compositeDescription = nil;
         [errorView release];
     } else {
         [self.waitView setHidden:false];
-        [self addProxyConfig:username password:password domain:caspianDomain];
+        [self addProxyConfig:[self cleanUsername:username] password:password domain:caspianDomain];
     }
 }
 
@@ -1096,6 +1096,30 @@ static UICompositeViewDescription *compositeDescription = nil;
         }
     }
     return YES;
+}
+
+
+#pragma mark - Private
+
+- (NSString *)removePrefix:(NSString *)prefix fromString:(NSString *)string {
+    if ([string hasPrefix:prefix]) {
+        return [string stringByReplacingOccurrencesOfString:prefix withString:@"" options:0 range:NSMakeRange(0, prefix.length)];
+    }
+    return string;
+}
+
+- (NSString *)cleanUsername:(NSString *)username {
+    NSArray *badPrefixes = @[@"+", @"00"];
+    
+    NSString *cleanUsername = username;
+    NSString *previousUsername = @"";
+    do {
+        previousUsername = cleanUsername;
+        for (NSString *prefix in badPrefixes) {
+            cleanUsername = [self removePrefix:prefix fromString:cleanUsername];
+        }
+    } while (![cleanUsername isEqual:previousUsername]);
+    return cleanUsername;
 }
 
 @end
