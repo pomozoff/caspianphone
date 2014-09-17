65191f374e32a79b170e793159d907beb244ea82
Anton Pomozov <pomozoff@me.com>
2014-09-17 13:50:57 +0400

    [Ref]: Minimizing changes to vanilla code

--- Classes/WizardViewController.m
+++ Classes/WizardViewController.m
@@ -453,19 +453,20 @@ static UICompositeViewDescription *compositeDescription = nil;
     LinphoneAddress* linphoneAddress = linphone_address_new(identity);
     linphone_address_set_username(linphoneAddress, normalizedUserName);
 
-    /*
-     This code cuts off specefied transport type from uri
-     for example here is setting from xml config:
-         sip:212.159.80.157:5060;transport=tcp
-     this code changes uri to:
-         sip:212.159.80.157
-     
     if( domain && [domain length] != 0) {
         // when the domain is specified (for external login), take it as the server address
-        linphone_proxy_config_set_server_addr(proxyCfg, [domain UTF8String]);
+
+        /*
+         This line of code cuts off specefied transport type in uri from xml config
+         for example here is setting from xml config:
+         sip:212.159.80.157:5060;transport=tcp
+         this line changes uri to:
+         sip:212.159.80.157
+        */
+        //linphone_proxy_config_set_server_addr(proxyCfg, [domain UTF8String]);
+        
         linphone_address_set_domain(linphoneAddress, [domain UTF8String]);
     }
-    */
 
     identity = linphone_address_as_string_uri_only(linphoneAddress);
 
