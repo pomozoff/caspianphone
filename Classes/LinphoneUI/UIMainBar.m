272dc573b3fb386bc4c5094820de3edd23bf0cf4
Anton Pomozov <pomozoff@me.com>
2014-10-16 14:01:33 +0400

    [Fix]: Linphone does not show unread messages right after run the app

--- Classes/LinphoneUI/UIMainBar.m
+++ Classes/LinphoneUI/UIMainBar.m
@@ -90,6 +90,13 @@ static NSString * const kDisappearAnimation = @"disappear";
                                                  name:kLinphoneSettingsUpdate
                                                object:nil];
     [self update:FALSE];
+    
+    // Fix linphone bug updating unread messages
+    double delayInSeconds = 1.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self updateUnreadMessage:YES];
+    });
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
