285417ff116f961ca1ec3d6e7d0f76f0fe005990
Anton Pomozov <pomozoff@me.com>
2014-10-27 17:35:30 +0300

    [Rem]: Email address of linphone developers

--- Classes/SettingsViewController.m
+++ Classes/SettingsViewController.m
@@ -828,7 +828,7 @@ static UICompositeViewDescription *compositeDescription = nil;
     } else if([key isEqual:@"about_button"]) {
         [[PhoneMainView instance] changeCurrentView:[AboutViewController compositeViewDescription] push:TRUE];
 	} else if ([key isEqual:@"send_logs_button"]) {
-        char * filepath = NULL; //linphone_core_compress_log_collection([LinphoneManager getLc]);
+        char * filepath = linphone_core_compress_log_collection([LinphoneManager getLc]);
 		if (filepath == NULL) {
 			[LinphoneLogger log:LinphoneLoggerError format:@"Cannot sent logs: file is NULL"];
 			return;
@@ -888,7 +888,7 @@ static UICompositeViewDescription *compositeDescription = nil;
 		picker.mailComposeDelegate = self;
 
 		[picker setSubject:NSLocalizedString(@"Linphone Logs",nil)];
-		[picker setToRecipients:[NSArray arrayWithObjects:@"linphone-iphone@belledonne-communications.com", nil]];
+		[picker setToRecipients:[NSArray arrayWithObjects:@"", nil]];
 		[picker setMessageBody:NSLocalizedString(@"Linphone logs", nil) isHTML:NO];
 		[picker addAttachmentData:attachment mimeType:type fileName:attachmentName];
 
