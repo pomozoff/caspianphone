414c7de656f3f75ed9ed06eb60259aa108477f5e
Anton Pomozov <pomozoff@gmail.com>
2015-01-08 04:19:09 +0300

    [Fix]: Removed warning about unused variable

--- Classes/SettingsViewController.m
+++ Classes/SettingsViewController.m
@@ -365,7 +365,6 @@
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
     [UINavigationControllerEx removeBackground:viewController.view];
-    UIViewController *oldTopViewController = self.topViewController;
 
     [viewController viewWillAppear:animated]; // Force view
     UILabel *labelTitleView = [[UILabel alloc] init];
