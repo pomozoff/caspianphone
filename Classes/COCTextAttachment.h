5e97fabef7b581d5dd9d17e869e8fd848965671b
Anton Pomozov <pomozoff@gmail.com>
2015-02-08 19:30:20 +0300

    [Add]: Calss to calculate size of smileys, GitHub issue #103

---
+++ Classes/COCTextAttachment.h
@@ -0,0 +1,13 @@
+//
+//  COCTextAttachment.h
+//  linphone
+//
+//  Created by Антон on 05.02.15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface COCTextAttachment : NSTextAttachment
+
+@end
---
+++ Classes/COCTextAttachment.m
@@ -0,0 +1,35 @@
+//
+//  COCTextAttachment.m
+//  linphone
+//
+//  Created by Антон on 05.02.15.
+//
+//
+
+#import "COCTextAttachment.h"
+
+@implementation COCTextAttachment
+
+- (id)initWithData:(NSData *)contentData ofType:(NSString *)uti {
+    self = [super initWithData:contentData ofType:uti];
+    if (self) {
+        if (self.image == nil) {
+            self.image = [UIImage imageWithData:contentData];
+        } else {
+            NSLog(@"COCTextAttachment - self.image is NOT nil");
+        }
+    }
+    return self;
+}
+- (CGRect)attachmentBoundsForTextContainer:(NSTextContainer *)textContainer proposedLineFragment:(CGRect)lineFrag glyphPosition:(CGPoint)position characterIndex:(NSUInteger)charIndex {
+    return [self scaleImageSizeToWidth:lineFrag.size.width];
+}
+
+- (CGRect)scaleImageSizeToWidth:(float)width {
+    float scalingFactor = 0.5;
+    CGSize imageSize = [self.image size];
+    CGRect rect = CGRectMake(0, 0, imageSize.width * scalingFactor, imageSize.height * scalingFactor);
+    return rect;
+}
+
+@end
