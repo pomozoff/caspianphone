4ffe907dfb81cffbb2d5b7e15bb5d19c15d37b65
Anton Pomozov <pomozoff@gmail.com>
2015-04-05 21:49:01 +0300

    [Ref]: Normalize phone number before making sip uri

--- Classes/Utils/FastAddressBook.m
+++ Classes/Utils/FastAddressBook.m
@@ -117,7 +117,7 @@ extern NSString *caspianDomainOldIpLocal;
     if ([phoneNumber hasPrefix:@"sip:"] || [phoneNumber rangeOfString:@"@"].location != NSNotFound) {
         return phoneNumber;
     }
-    return [NSString stringWithFormat:@"sip://%@@%@", phoneNumber, caspianDomainIpLocal];
+    return [NSString stringWithFormat:@"sip://%@@%@", [self normalizePhoneNumber:phoneNumber], caspianDomainIpLocal];
 }
 
 + (NSString*)normalizeSipURI:(NSString*)address {
