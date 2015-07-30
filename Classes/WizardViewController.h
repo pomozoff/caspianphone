74d440810e55fb95d914df54421b1529a15034b2
Pavel Anikin <apvex@me.com>
2015-07-30 14:36:04 +0300

    [Add]: creation new graphic screens for SignUp and Forget Password Views

--- Classes/WizardViewController.h
+++ Classes/WizardViewController.h
@@ -42,7 +42,12 @@
 @property (nonatomic, retain) IBOutlet UIScrollView *contentView;
 
 @property (nonatomic, retain) IBOutlet UIView *welcomeView;
+@property (retain, nonatomic) IBOutlet UIView *countrySignUpView;
+@property (retain, nonatomic) IBOutlet UIView *phoneNumberSignUpView;
 @property (nonatomic, retain) IBOutlet UIView *signUpView;
+
+@property (retain, nonatomic) IBOutlet UIView *getActivationByCodeView;
+
 @property (nonatomic, retain) IBOutlet UIView *phoneNumberFoundView;
 @property (nonatomic, retain) IBOutlet UIView *phoneNumberExistsView;
 @property (nonatomic, retain) IBOutlet UIView *passwordReceivedView;
